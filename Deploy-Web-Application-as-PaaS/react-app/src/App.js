6eef7d333e32b54bca23ae66483acb131dc5884b
Sharayu Salokhe <108818099+Sharayu2@users.noreply.github.com>
2023-10-23 12:10:14 +0530

    Update App.js

--- Deploy-Web-Application-as-PaaS/react-app/src/App.js
+++ Deploy-Web-Application-as-PaaS/react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first React Application
+         My First React Application 
         </p>
         <a
           className="App-link"
